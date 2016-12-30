28645ce6f3f9c6f1655c1e2ea4ca5bea8988a365
张一帆 <740253664@qq.com>
2016-12-30 15:42:49 +0800

    版本更新： 还原端口

--- public/app/js/config.js
+++ public/app/js/config.js
@@ -3,14 +3,14 @@ var my_config = {
         // 通讯服务器地址
         //communication_server_host: window.location.href
         //communication_server_host: 'ws://10.0.8.101:8081/websocket?data=eyJncm91cF9pZCI6IjIyMjIyMiIsImN1c3RvbWVyX2lkIjoiMTExMTExIiwidG9rZW4iOiJjZjRmZDg4OGI1MjhlNzkzMzMyZGMyMTM1NGU4OTJlYjMyYTA1ZWE3ZTM0OGZiNmVmOTJjYjJhNGQyNTg5MTlmIn0='
-        communication_server_host: 'ws://csws.17youyun.com/websocket',
+        communication_server_host: 'ws://csws.17youyun.com:8097/websocket',
         // communication_server_host: 'ws://192.168.33.191:8097/websocket?data=eyJncm91cF9pZCI6IjIyMjIyMiIsImN1c3RvbWVyX2lkIjoiMTExMTExIiwidG9rZW4iOiJjZjRmZDg4OGI1MjhlNzkzMzMyZGMyMTM1NGU4OTJlYjMyYTA1ZWE3ZTM0OGZiNmVmOTJjYjJhNGQyNTg5MTlmIn0='
         //upload: 'http://10.0.8.91:8096/fileProcess/custUploadFile',
-        upload: 'http://csapi.17youyun.com/fileProcess/custUploadFile',
+        upload: 'http://csws.17youyun.com:8096/fileProcess/custUploadFile',
         //upload: 'http://localhost:3000/file/upload',
-        history: 'http://csapi.17youyun.com/history/getHistory',
+        history: 'http://csws.17youyun.com:8096/history/getHistory',
         //login: 'http://csws.17youyun.com:8096/customer/login',
-        login: 'http://csapi.17youyun.com/customer/login',
+        login: 'http://csws.17youyun.com:8096/customer/login',
     },
     avatar:{
         kf:'/public/app/img/avatar/kfavatar.png',
